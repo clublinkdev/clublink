d28edfaa9e1c6057f8001ef66ce4041bb16a5249
JOSH-Configs <67236442+JOSH-Configs@users.noreply.github.com>
2022-01-02 16:20:44 +0000

    Update sequences.ts

--- src/resources/sequences.ts
+++ src/resources/sequences.ts
@@ -1,7 +1,7 @@
 export default {
     ADMIN_TITLE: "ꌄ섆魉쐂뜈ꈋ忧ꈂ匀ꈂ愃ꈂ慎ꈂ惋",
-    GUIDE_TITLE: "ꌄ섆魉쐁",
-    MOD_TITLE: "섆魉쌇뜆醋 ꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝ愃ꈂ愭ꈂ匀ꈂ",
+    BETA_TITLE: "ꌄ섆魉胩뜇穙慵ꈂ怡ꈂ恢ꈂ忧",
+    MOD_TITLE: "ꌄ섆魉쌇뜆醭愃ꈂ愭ꈂ匀",
     MANAGER_TITLE: "ꌄ섆魉쌈뜈ꈋ愃忧惋忧悈怡愤",
     OWNER_TITLE: "ꌄ섆魉쌆ꌄ뜈醋ꑝꑝ 愭ꈂ肄ꈂ惋ꈂ怡ꈂ愤",
 };
