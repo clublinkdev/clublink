1b0605cc6ba868f3d81ecf86f2aca4630dde3625
JOSH-Configs <67236442+JOSH-Configs@users.noreply.github.com>
2022-01-02 12:27:53 +0000

    All Staff Titles

--- src/resources/sequences.ts
+++ src/resources/sequences.ts
@@ -1,3 +1,7 @@
 export default {
-    ADMIN_TITLE: "ꌄ섆魉쐂뜈ꈋ忧ꈂ匀ꈂ愃ꈂ慎ꈂ惋"
-}
+    ADMIN_TITLE: "ꌄ섆魉쐂뜈ꈋ忧ꈂ匀ꈂ愃ꈂ慎ꈂ惋",
+    GUIDE_TITLE: "ꌄ섆魉쐁",
+    MOD_TITLE: "섆魉쌇뜆醋 ꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝꑝ愃ꈂ愭ꈂ匀ꈂ",
+    MANAGER_TITLE: "ꌄ섆魉쌈뜈ꈋ愃忧惋忧悈怡愤",
+    OWNER_TITLE: "ꌄ섆魉쌆ꌄ뜈醋ꑝꑝ 愭ꈂ肄ꈂ惋ꈂ怡ꈂ愤",
+};
