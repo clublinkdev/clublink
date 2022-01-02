5c354e011b2fd071a909e7911d4c14ed828f42a0
JOSH-Configs <67236442+JOSH-Configs@users.noreply.github.com>
2022-01-02 22:02:13 +0000

    Update emojis.ts

--- src/resources/emojis.ts
+++ src/resources/emojis.ts
@@ -1,5 +1,5 @@
 export default {
     MINECLUB_LOGO: "<:mineclub:827293117224976416>",
     CHRISTMAS_TOKEN: "<a:christmas_token_gif:923746377937326101>",
-    GEM: "<:Gem_100:837390660952653844>"
+    GEMS: "<:Gem_100:837390660952653844>"
 };
