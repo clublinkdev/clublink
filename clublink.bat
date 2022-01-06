f0ce220852faf18041c03567439da9fa46514bd3
JOSH-Configs <67236442+JOSH-Configs@users.noreply.github.com>
2022-01-06 23:34:44 +0000

    Update clublink.bat

--- clublink.bat
+++ clublink.bat
@@ -1,6 +1,7 @@
 if exist node_modules\ (
-	ts-node ./src/index.ts
+	ts-node ./src/index.ts --gui --chat
 ) else (
 	npm i
 	npm i -g typescript ts-node
+        ts-node ./src/index.ts --gui --chat
 )
