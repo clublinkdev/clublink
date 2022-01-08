8a2a19b98aae5958415e7c2866fc43169f3c7a14
JOSH-Configs <67236442+JOSH-Configs@users.noreply.github.com>
2022-01-08 15:27:23 +0000

    Update 3.0.1

--- src/resources/sequences.ts
+++ src/resources/sequences.ts
@@ -1,7 +1,8 @@
 export default {
     ADMIN_TITLE: "ꌄ섆魉쐂뜈ꈋ忧ꈂ匀ꈂ愃ꈂ慎ꈂ惋",
     BETA_TITLE: "ꌄ섆魉胩뜇穙慵ꈂ怡ꈂ恢ꈂ忧",
-    MOD_TITLE: "ꌄ섆魉쌇뜆醭愃ꈂ愭ꈂ匀",
+    MOD_TITLE: "ꌄ섆魉쌇",
     MANAGER_TITLE: "ꌄ섆魉쌈뜈ꈋ愃忧惋忧悈怡愤",
-    OWNER_TITLE: "ꌄ섆魉쌆ꌄ뜈醋ꑝꑝ 愭ꈂ肄ꈂ惋ꈂ怡ꈂ愤",
+    OWNER_TITLE: "ꌄ섆魉쌆뜈ꈋ愭ꈂ肄ꈂ惋ꈂ怡ꈂ愤",
+    SUS_TITLE: "ꌄ섆魉叆뜆醭憭ꈂ匆ꈂ憭",
 };
