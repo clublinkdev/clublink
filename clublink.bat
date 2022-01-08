17419f9a12529a255233f15b381580c3c4e3f1ea
Nick <82282004+xploree@users.noreply.github.com>
2022-01-08 16:46:53 -0500

    Add files via upload

--- clublink.bat
+++ clublink.bat
@@ -1,9 +1,10 @@
+node updating/update.js
+
+pause 
 if exist node_modules\ (
 	ts-node ./src/index.ts --gui --chat
-	pause
 ) else (
 	npm i
 	npm i -g typescript ts-node
-	ts-node ./src/index.ts --gui --chat
-	pause
+        ts-node ./src/index.ts --gui --chat
 )
