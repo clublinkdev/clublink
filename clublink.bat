d83f61e5b3fa222913a8e6a77f950e092714abfc
hanatic <johndoe@example.com>
2022-01-08 16:03:53 +0000

    Please for the love of god work

--- clublink.bat
+++
@@ -1,9 +0,0 @@
-if exist node_modules\ (
-	ts-node ./src/index.ts --gui --chat
-	pause
-) else (
-	npm i
-	npm i -g typescript ts-node
-	ts-node ./src/index.ts --gui --chat
-	pause
-)
--- src/resources/emojis.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    MINECLUB_LOGO: "<:mineclub:827293117224976416>",
-    ANNIVERSARY_TOKEN: "<a:anniversarytoken:929131428376105021>",
-    GEMS: "<:Gem_100:837390660952653844>"
-};
-
