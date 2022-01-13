47c5c1dd11fd303dadc261b9fdfb8db4473f0652
Rihand Creative <97323467+MasterDee22@users.noreply.github.com>
2022-01-13 09:22:29 +0700

    Improved Startup File
    - Created cross-platform Bash startup file (compatible with Windows, Mac, and Linux)
    - Updated Windows Batch file to only open the Bash file instead of starting up on its own.
    - Added option to install the node modules with sudo privileges.
    - Added option to use --gui and --gg parameter without having to add them manually.

---
+++ clublink.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+# If "node_modules" folder exists
+if [ -d "./node_modules" ] 2>/dev/null
+then
+	read -r -p "Do you want to view data about GUIs that are opened? [Y/n] " input
+	case $input in
+		[yY][eE][sS]|[yY])
+			read -r -p "Do you want to send GG's when people buy boxes from store.mineclub.com? [Y/n] " input1
+			case $input1 in
+				[yY][eE][sS]|[yY])
+					ts-node ./src/index.ts --gui --gg
+				;;
+				[nN][oO]|[nN])
+					ts-node ./src/index.ts --gui
+				;;
+			*)
+				echo "Invalid input!"
+				exit
+				;;
+			esac
+			;;
+			
+		[nN][oO]|[nN])
+			read -r -p "Do you want to send GG's when people buy boxes from store.mineclub.com? [Y/n] " input2
+			case $input2 in
+			yY][eE][sS]|[yY])
+					ts-node ./src/index.ts --gg
+				;;
+				[nN][oO]|[nN])
+					ts-node ./src/index.ts
+				;;
+			*)
+				echo "Invalid input!"
+				exit
+				;;
+			esac
+			;;
+	*)
+		echo "Invalid input!"
+		exit
+		;;
+	esac
+	
+# If "node_modules" folder not exists
+else
+	echo "Modules not detected. Application hasn't been installed."
+	# If using WindowsOS, skip sudo prompt
+	if [ "$OSTYPE" == "msys" ] || [ "$OSTYPE" == "cygwin" ]
+	then
+		read -r -p "Do you wish to run the installation? [Y/n] " input
+		case $input in
+			[yY][eE][sS]|[yY])
+				npm i
+				npm i -g typescript ts-node
+			;;
+			[nN][oO]|[nN])
+				exit
+			;;
+		*)
+			echo "Invalid input!"
+			exit
+			;;
+		esac
+
+	else
+		read -r -p "Do you wish to run the installation with sudo/root privileges? (recommended for Linux) [Y/n] " input
+		case $input in
+			[yY][eE][sS]|[yY])
+				sudo npm i
+				sudo npm i -g typescript ts-node
+			;;
+			[nN][oO]|[nN])
+				npm i
+				npm i -g typescript ts-node
+			;;
+		*)
+			echo "Invalid input!"
+			exit
+			;;
+		esac
+
+	fi
+	
+	echo ""
+	echo ""
+	echo "All modules have been installed. Adjust the .env file, then run this file again."
+fi
