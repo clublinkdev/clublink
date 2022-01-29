55a02300787604cbec8610ffe23ff66fe630fb5c
Diligord1 <80379684+Diligord1@users.noreply.github.com>
2022-01-29 16:53:42 +1100

    Update sequences.ts
    - Added married and celestial title to the list
    - Fixed titles not showing on ClubLink

--- src/resources/sequences.ts
+++ src/resources/sequences.ts
@@ -1,9 +1,11 @@
 export default {
-    ADMIN_TITLE: "忧匀愃慎惋",
-    BETA_TITLE: "慵怡恢忧",
+    ADMIN_TITLE: "쐂뜈ꈋ",
+    BETA_TITLE: "慷ꈂ匆ꈂ恢ꈂ怡",
     MOD_TITLE: "섆魉쌇",
-    MANAGER_TITLE: "愃忧惋忧悈怡愤",
-    OWNER_TITLE: "愭肄惋怡愤",
-    SUS_TITLE: "憭匆憭",
-    PRIDE_TITLE: "鸨愤慎匀怡"
+    MANAGER_TITLE: "愃ꈂ忧ꈂ惋ꈂ忧ꈂ悈ꈂ怡ꈂ愤",
+    OWNER_TITLE: "愭ꈂ肄ꈂ惋ꈂ怡ꈂ愤",
+    SUS_TITLE: "憭ꈂ匆ꈂ憭",
+    PRIDE_TITLE: "鸨ꈂ愤ꈂ慎ꈂ匀ꈂ怡",
+    MARRIED_TITLE: "愃ꈂ忧ꈂ愤ꈂ愤ꈂ慎ꈂ怡ꈂ匀",
+    CELESTIAL_TITLE: "慷ꈂ怡ꈂ悰ꈂ怡ꈂ憭ꈂ恢ꈂ慎ꈂ忧ꈂ悰"
 };
