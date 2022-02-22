f77742c07a5c9c71b654e2e11d7a46410a040463
JOSH-Configs <67236442+JOSH-Configs@users.noreply.github.com>
2022-02-22 19:47:19 +0000

    Auto GG Added

--- clublink.bat
+++ clublink.bat
@@ -1,9 +1,9 @@
 if exist node_modules\ (
-	ts-node ./src/index.ts --gui --chat
+	ts-node ./src/index.ts --gui --chat --gg
 	pause
 ) else (
 	npm i
 	npm i -g typescript ts-node
-	ts-node ./src/index.ts --gui --chat
+	ts-node ./src/index.ts --gui --chat --gg
 	pause
 )
